1d2688a5294fded4d5df7a0fa63a88565b8ad723
Amin Babadi <amin.babadi89@gmail.com>
2024-01-27 15:54:45 +0200

    Chicken base

---
+++ Assets/Scripts/Chicken.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    PlayerController owner;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetOwner(PlayerController _owner)
+    {
+        owner = _owner;
+    }
+}
