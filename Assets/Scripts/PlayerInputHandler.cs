b4d7c0d02021a5a98b529bf5de747fe289439a7e
Mahdi Hamdarsi <mahdi.hamdarsi@rovio.com>
2024-01-27 17:01:09 +0200

    Input handler implementation

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ModestTree;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class InputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private ControllerInputActions controllerInputActions;
+    public int playerNumber;
+
+    private List<KeyCode> keyboardKeys = new List<KeyCode>();
+
+    void Start()
+    {
+       controllerInputActions = GetComponent<ControllerInputActions>();
+
+       if (playerNumber == 1)
+       {
+           keyboardKeys.Add(KeyCode.W);
+           keyboardKeys.Add(KeyCode.S);
+           keyboardKeys.Add(KeyCode.A);
+           keyboardKeys.Add(KeyCode.D);
+           keyboardKeys.Add(KeyCode.Space);
+       }
+       else if (playerNumber == 2)
+       {
+           keyboardKeys.Add(KeyCode.UpArrow);
+           keyboardKeys.Add(KeyCode.DownArrow);
+           keyboardKeys.Add(KeyCode.LeftArrow);
+           keyboardKeys.Add(KeyCode.RightArrow);
+           keyboardKeys.Add(KeyCode.Return);
+       }
+    }
+
+    void Update()
+    {
+        var actionPressed = controllerInputActions.Player.Action.IsPressed();
+        var value = controllerInputActions.Player.Direction.ReadValue<Vector2>();
+
+        var vector = new Vector3
+        {
+            z = value.x,
+            x = -value.y
+        };
+
+        if (!keyboardKeys.IsEmpty())
+        {
+            vector.x += Input.GetKey(keyboardKeys[0]) ? -1 : 0;
+            vector.x += Input.GetKey(keyboardKeys[1]) ? 1 : 0;
+            vector.z += Input.GetKey(keyboardKeys[2]) ? -1 : 0;
+            vector.z += Input.GetKey(keyboardKeys[3]) ? 1 : 0;
+
+            if (Input.GetKeyDown(keyboardKeys[4]))
+                actionPressed = true;
+        }
+        
+               
+    }
+}
