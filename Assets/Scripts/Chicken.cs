250e75e874d7d1c1731e2ff5def57d554dedb621
Amin Babadi <amin.babadi89@gmail.com>
2024-01-28 10:12:35 +0200

    Fixed pivot points and improved physics

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -6,6 +6,10 @@ public class Chicken : MonoBehaviour
 {
     PlayerController owner;
 
+    float timer = 0f;
+    public float ownedRotationSpeed = 5f;
+    public float ownedRotationRadius = 1f;
+
     void Start()
     {
         
@@ -15,13 +19,18 @@ public class Chicken : MonoBehaviour
     {
         if(owner != null)
         {
-            transform.position = owner.transform.position + new Vector3(0f, 0f, 1f);
-            // Debug.DrawLine(owner.transform.position, owner.transform.position + Vector3.up, Color.yellow);
+            Vector3 offset = new Vector3 (Mathf.Sin(ownedRotationSpeed * timer), 0f, Mathf.Cos(ownedRotationSpeed * timer));
+            transform.position = owner.transform.position + ownedRotationRadius * offset;
+            timer += Time.deltaTime;
         }
     }
 
     public void SetOwner(PlayerController _owner)
     {
         owner = _owner;
+
+        timer = 0f;
+
+        GetComponentInChildren<Collider>().enabled = false;
     }
 }
